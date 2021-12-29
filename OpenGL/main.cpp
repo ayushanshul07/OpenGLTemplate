8a73d8615bf705a392a7946f0878c6fbcf2ba241
Ayush Anshul <ayushanshul07@gmail.com>
2021-12-29 16:13:19 +0530

    Initial Commit

---
+++ OpenGL/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  OpenGL
+//
+//  Created by Ayush Anshul on 29/12/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
